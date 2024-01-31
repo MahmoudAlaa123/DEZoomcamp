8212d8c6ca9a97ad508d55ef0dd5c2a9dfd14a89
Mahmoud Alaa <62549876+MahmoudAlaa123@users.noreply.github.com>
2024-01-31 23:00:20 +0200

    Update week1_hw.sql

--- week1_hw.sql
+++ week1_hw.sql
@@ -1,14 +1,15 @@
+-- Question 3
 SELECT count(lpep_pickup_datetime)
 	FROM public.green_taxi_trips_hw
 	WHERE date(lpep_pickup_datetime)='2019-09-18' and date(lpep_pickup_datetime)='2019-09-18';
 	
-	
+-- Question 4	
 SELECT date(lpep_pickup_datetime), max(trip_distance) max_trip_distance
 FROM public.green_taxi_trips_hw
 GROUP BY date(lpep_pickup_datetime)
 ORDER BY max_trip_distance desc;
 
-
+-- Question 5
 SELECT "Borough",date(lpep_pickup_datetime), SUM(total_amount) as sum_total_amount 
 FROM public.green_taxi_trips_hw 
 LEFT JOIN public.taxi_zone_hw 
@@ -20,7 +21,7 @@ ORDER BY sum_total_amount DESC
 
 
 
-
+-- Question 6
 with PU_Astoria as(
 SELECT "PULocationID","DOLocationID","Zone",tip_amount
 FROM public.green_taxi_trips_hw 
